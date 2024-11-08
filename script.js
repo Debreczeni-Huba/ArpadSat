2fcfdabae515cf983912cfdc347b4bfc2d30f73a
Debreczeni-Huba <112760835+Debreczeni-Huba@users.noreply.github.com>
2024-11-08 19:18:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ function respat2() {
 	restat2.classList.remove('active-checkresp');
 	header2.classList.remove('active-checkresp');
 }
+
+const gb = document.querySelector('.gb');
+const hb = document.querySelector('.hb');
+const mb = document.querySelector('.mb');
+const db = document.querySelector('.db');
+const zsb = document.querySelector('.zsb');
+const bb = document.querySelector('.bb');
+const openg = document.querySelector('.openg');
+const openh = document.querySelector('.openh');
+const openm = document.querySelector('.openm');
+const opend = document.querySelector('.opend');
+const openzs = document.querySelector('.openzs');
+const openb = document.querySelector('.openb');
+const iconClose = document.querySelector('.icon-close');
+
+openg.addEventListener('click', ()=> {
+    gb.classList.add('active-popup');
+});
+openh.addEventListener('click', ()=> {
+    hb.classList.add('active-popup');
+});
+openm.addEventListener('click', ()=> {
+    mb.classList.add('active-popup');
+});
+opend.addEventListener('click', ()=> {
+    db.classList.add('active-popup');
+});
+openzs.addEventListener('click', ()=> {
+    zsb.classList.add('active-popup');
+});
+openb.addEventListener('click', ()=> {
+    bb.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+    window.classList.remove('active-popup');
+});
